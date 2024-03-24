272f0e2eb81ecce4664f87fa403a5f3fa3480d46
Lafourcade-Noah <116797848+Lafourcade-Noah@users.noreply.github.com>
2024-03-24 17:51:45 +0100

    routerlink modif

--- src/components/cardprojets.vue
+++ src/components/cardprojets.vue
@@ -1,4 +1,5 @@
 <script>
+import { RouterLink } from 'vue-router';
 import { projects } from '../project.js';
 
 export default {
@@ -28,19 +29,19 @@ export default {
                             <h2 class="text-5xl font-semibold mb-2">{{ project.title }}</h2>
                             <div class=" flex flex-wrap space-x-3">
                                 <div class="tag">{{ project.tag1 }}</div>
-                                <div class="tag" v-show="project.tag2">{{ project.tag2 }}</div>
+                                <div class="tag" v-if="project.tag2">{{ project.tag2 }}</div>
                             </div>
                             <p class=" font-extralight">
                                 {{ project.description }}
                             </p>
                             <button>
-                                <a :href="project.projetUrl" class="relative inline-flex items-center pr-12 py-1 overflow-hidden text-lg font-medium    hover:text-white group hover:white">
+                                <RouterLink :to="project.projetUrl" class="relative inline-flex items-center pr-12 py-1 overflow-hidden text-lg font-medium    hover:text-white group hover:white">
 <span class="absolute left-0 block w-full h-0 transition-all group-hover:h-full top-1/2 group-hover:top-0 duration-400 ease"></span>
 <span class="absolute right-0 flex items-center justify-start w-10 h-10 duration-300 transform translate-x-full group-hover:translate-x-0 ease">
 <svg class="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M14 5l7 7m0 0l-7 7m7-7H3"></path></svg>
 </span>
 <span class="relative">Voir tout le projet</span>
-</a>
+                                </RouterLink>
                             </button>
                         </div>
                     </div>
